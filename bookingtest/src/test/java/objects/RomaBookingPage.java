4854de29129712787b0ab438cbe85331b5708157
BLRoman <roman.balykhin@gmail.com>
2015-06-10 19:17:49 +0300

    New Page and Tests

--- bookingtest/src/test/java/objects/RomaBookingPage.java
+++ bookingtest/src/test/java/objects/RomaBookingPage.java
@@ -1,15 +1,23 @@
 package objects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
 
 public class RomaBookingPage {
 	
+	WebDriver driver;
 
-	@FindBy (name="station_from")
+	@FindBy (xpath="//input[@name='station_from']")
 	private WebElement station_from;
 	
-	@FindBy (name="station_till")
+	@FindBy (xpath="//input[@name='station_till']")
 	private WebElement station_till;
 	
 	@FindBy (name="date_dep")
@@ -18,5 +26,57 @@ public class RomaBookingPage {
 	@FindBy (name="search")
 	private WebElement search;
 	
+	@FindBy (xpath="//*[@id='ts_res_tbl']")
+	public WebElement tTable;
+	
+	 public RomaBookingPage(WebDriver driver) 
+	 {
+			this.driver = driver;
+			PageFactory.initElements(driver, this);
+	 }
 
+	public void enterStationFrom(String value) throws InterruptedException
+	{	
+		station_from.sendKeys(value);
+		WebDriverWait wait = new WebDriverWait(driver, 4);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='stations_from']")));
+		driver.findElement(By.xpath(".//*[@id='stations_from']/div[1]")).click();;
+		
+	}
+	
+	public void enterStationTill(String value) throws InterruptedException
+	{
+		station_till.sendKeys(value);
+	    WebDriverWait wait = new WebDriverWait(driver, 4);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='stations_till']")));
+		driver.findElement(By.xpath(".//*[@id='stations_till']/div[1]")).click();;
+	}
+	
+	public void trainsTable()
+	{
+		WebDriverWait wait = new WebDriverWait(driver, 4);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='ts_res']")));
+		
+		
+	}
+	
+	public void enterDate(String value)
+	{
+		date_dep.clear();
+		date_dep.sendKeys(value);
+	}
+	
+	public void submitbuttonSearch()
+	{
+	     search.click();
+	}
+	
+	public void cleanUpAll()
+	{
+		station_from.clear();
+		station_till.clear();
+		date_dep.clear();
+		
+	}
+	
 }
--- bookingtest/src/test/java/tests/Roma_Test.java
+++ bookingtest/src/test/java/tests/Roma_Test.java
@@ -1,12 +1,68 @@
 package tests;
 
+import objects.RomaBookingPage;
+
+import org.openqa.selenium.By;
+//import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Roma_Test {
+
+	WebDriver driver;
+	RomaBookingPage bookingPage;
+	String from = "Kyiv";
+	String till = "Odesa";
+	String date = "06.12.2015";
 	
+	
+	@BeforeClass
+	public void startUp() {
+
+		driver = new FirefoxDriver();
+		driver.get("http://booking.uz.gov.ua/en/");
+		driver.manage().window().maximize();
+		bookingPage=PageFactory.initElements(driver,RomaBookingPage.class);
+
+	}
+
 	@Test
-    public void RomanTest() {
-	System.out.println("Roma test");
-    }
+	public void RomanTestTableIsPresent() throws InterruptedException {
+		
+		bookingPage.enterStationFrom(from);
+		bookingPage.enterStationTill(till);
+		bookingPage.enterDate(date);
+		bookingPage.submitbuttonSearch();
+		bookingPage.trainsTable();
+		
+		Assert.assertEquals(bookingPage.tTable, driver.findElement(By.xpath("//*[@id='ts_res_tbl']")));
+		
+							
+	}
+
+	@Test
+	public void RomanTestTrainIsPresent() throws InterruptedException {
+		
+		bookingPage.cleanUpAll();
+		bookingPage.enterStationFrom(from);
+		bookingPage.enterStationTill(till);
+		bookingPage.enterDate("06.12.2015");
+		bookingPage.submitbuttonSearch();
+		bookingPage.trainsTable();
+	
+		Assert.assertEquals(driver.findElement(By.xpath(".//*[@class='num']/a")).getText(), "763 Ê");
+		
+	}	
+	
+	@AfterClass
+	public void shutDown() {
+		driver.quit();
+	}
 
 }
